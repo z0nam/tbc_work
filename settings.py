c00e3e736261126d22ea231e05e180d24f6bb778
Namun Cho <namun.cho@gmail.com>
2020-12-02 23:06:50 +0900

    kberi session deleted

--- settings.py
+++ settings.py
@@ -3,16 +3,6 @@ from os import environ
 SESSION_CONFIG_DEFAULTS = {"real_world_currency_per_point": 67, "participation_fee": 1}
 SESSION_CONFIGS = [
     {
-        "name": "VR_AR_KBERI",
-        "display_name": "신SW인식조사(현장실험)",
-        "num_demo_participants": 1,
-        "app_sequence": [
-            "vrar",
-            "iat",
-            "ending_behavioral",
-        ]
-    },
-    {
         "name": "VR_AR_embrain",
         "display_name": "신SW인식조사(엠브레인)",
         "num_demo_participants": 1,
